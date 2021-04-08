e52aaf39146163f4a9478dc3bcc87fb2db86c510
Vento <vento.michael0705@hotmail.com>
2021-04-08 09:31:00 +0200

    add views connect

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
     $categories = \App\Models\Category::orderBy('name')->get();
     return view('home.index',compact('users','categories'));
 });
+
+Route::get('/login', [\App\Http\Controllers\DashboardController::class, 'index'])->middleware('auth')->name('dashboardUser.index');
+
